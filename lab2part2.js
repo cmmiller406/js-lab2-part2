c6cef6884843ad05a0414c2b44406eea823bb501
christina <cmmiller.406@gmail.com>
2019-01-02 14:29:56 -0500

    fixed functionality

--- lab2part2.js
+++ lab2part2.js
@@ -1,77 +1,65 @@
 "use strict";
-
 class AddressBook {
-    constructor() {
-        this.contacts = [
-            new Contact("bob", "bob@bob.com", "111", "weird neighbor"),
-            new Contact("steve", "steve@bob.com", "111", "uncle"),
-            new Contact("sally", "sally@bob.com", "111", "mom"),
-        ]
+  constructor() {
+    this.contacts = [
+      { name: "Adam", email: "adam@grandcircus.co", phone: "111-222-3333", relation: "friend" },
+      { name: "Shelly", email: "shelly@grandcircus.co", phone: "111-222-3333", relation: "family" },
+      { name: "David", email: "david@grandcircus.co", phone: "111-222-3333", relation: "family" },
+      { name: "Bob", email: "bob@grandcircus.co", phone: "111-222-3333", relation: "friend" },
+      { name: "Megan", email: "megan@grandcircus.co", phone: "111-222-3333", relation: "friend" },
+      { name: "Julie", email: "julie@grandcircus.co", phone: "111-222-3333", relation: "coworker" },
+      { name: "Jon", email: "jon@grandcircus.co", phone: "111-222-3333", relation: "coworker" }
+    ];
+  }
+  add() {
+    const info = {
+      name: document.querySelectorAll(".contact__input")[0].value, 
+      email: document.querySelectorAll(".contact__input")[1].value, 
+      phone: document.querySelectorAll(".contact__input")[2].value, 
+      relation: document.querySelectorAll(".contact__input")[3].value
+    };
+    const newContact = new People(info.name, info.email, info.phone, info.relation);
+    addressBook.contacts.push(newContact);
+    this.display();
+  }
+  delete(e) {
+    addressBook.contacts.splice(e.target.parentElement.parentElement.getAttribute("data-index"), 1);
+    addressBook.display();
+  }
+  display() {
+    document.querySelector(".contact__list").innerHTML = "";
+    let count = 0;
+    for (let person of this.contacts) {
+      const newEntry = document.createElement("section");
+      console.dir(newEntry);
+      newEntry.setAttribute("data-index", count);
+      newEntry.classList.add("contact__box");
+      newEntry.innerHTML = `
+      <ul class="contact__box__data">
+        <li>Name: ${person.name}</li> 
+        <li>Email: ${person.email}</li>
+        <li>Phone: ${person.phone}</li>
+        <li>Relation: ${person.relation}</li>
+      </ul>
+      <button type="button" class="contact__delete"><i class="material-icons">&#xE872;</i></button>
+      `;
+      document.querySelector(".contact__list").appendChild(newEntry);
+      count++;
     }
-    add() {
-        this.contacts.push();
+    for (let i = 0; i < addressBook.contacts.length; i++) {
+      document.querySelectorAll(".contact__delete")[i].addEventListener("click", addressBook.delete);
     }
-    deleteAt() {
-        this.contacts.splice(index, 1);
-    }
-    display() {
-        this.contacts.push();
-    }
-};
+  }
+}
 
-class Contact {
-    constructor(name, email, phone, relation) {
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.relation = relation;
-    }
-};
+class People {
+  constructor(name, email, phone, relation) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.relation = relation;
+  }
+}
 
-const book = new AddressBook();
-
-// add new contact
-
-const add = () => {
-    document.querySelector("btn").addEventListener("click", () => {
-        const inputs = document.querySelectorAll("forminput");
-        contacts.push({
-            name: inputs[0].value,
-            email: inputs[1].value,
-            phone: input[2].value,
-            relations: input[3].value,
-        });
-    });
-    book.add(new Contact(name, email, phone, relation));
-    add();
-};
-
-// delete contact
-const deleteAt = () => {
-    const domContainer = document.querySelector(".dom-container");
-    document.querySelector("trash").addEventListener("click", () => {
-    domContainer.removeChild(p);
-    });
-};
-
-// display
-const display = () => {
-    const domContainer = document.querySelector(".dom-container");
-    const addButton = document.getElementById("btn");
-    domContainer.innerHTML = "";
-    addButton.addEventListener("click", () => {
-        for (let contact of book.contacts) {
-            const p = document.createElement("p");
-            p.innerText = `Name:${book.name} Email:${book.email} Phone:${book.phone} Relation:${book.relation}`;
-            domContainer.appendChild(p);
-            const trash = document.createElement("div");
-            trash.innerText = ``;
-            domContainer.appendChild(trash);
-        };
-    });
-};
-display();
-
-document.addEventListener("load", () => {
-    display();
-});
+const addressBook = new AddressBook();
+addressBook.display();
